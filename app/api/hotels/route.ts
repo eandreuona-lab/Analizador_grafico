7b518bd23abd08d75dd1321476290d6db225295a
eandreuona-lab <eandreuona@gmail.com>
2026-05-28 14:33:41 +0200

    Update route.ts

--- app/api/hotels/route.ts
+++ app/api/hotels/route.ts
@@ -1,1 +1,19 @@
+import { NextResponse } from "next/server";
+import fs from "fs";
+import path from "path";
 
+export async function GET() {
+  const dirPath = path.join(process.cwd(), "public/data");
+
+  const files = fs.readdirSync(dirPath);
+
+  const hotels = files
+    .filter((file) => file.endsWith(".xlsx"))
+    .map((file) => ({
+      name: file.replace(".xlsx", "").replace(/_/g, " "),
+      file: `/data/${file}`,
+    }));
+
+  return NextResponse.json(hotels);
+}
+``
